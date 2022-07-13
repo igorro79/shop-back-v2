25280f9d81f9113b3fe85d69b02e05528606fdaf
igorro79 <griniov.igor@gmail.com>
2022-07-13 17:06:36 +0300

    change port to 44680

--- index.js
+++ index.js
@@ -4,33 +4,31 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
-// const shopRoutes = require("./routes/api/shops-api");
-// const cartRoutes = require("./routes/api/carts-api");
+const shopRoutes = require("./routes/api/shops-api");
+const cartRoutes = require("./routes/api/carts-api");
 const test = require("./routes/test");
-
-const port = process.env.PORT || 5000;
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 
-// app.use("/api/shops", shopRoutes);
-// app.use("/api/carts", cartRoutes);
+app.use("/api/shops", shopRoutes);
+app.use("/api/carts", cartRoutes);
 app.use("/", test);
 
 async function start() {
   try {
-    // await mongoose
-    //   .connect(process.env.MONGO_URL)
-    //   .then(console.log("database connect succesfully"))
-    //   .catch((error) => {
-    //     console.log(error);
-    //     console.log(error.message);
-    //     process.exit(1);
-    //   });
+    await mongoose
+      .connect(process.env.MONGO_URL)
+      .then(console.log("database connect succesfully"))
+      .catch((error) => {
+        console.log(error);
+        console.log(error.message);
+        process.exit(1);
+      });
 
-    app.listen(port, () => {
-      console.log(`Server running. Use our API on port: ${port}`);
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running. Use our API on port: ${process.env.PORT}`);
     });
   } catch (error) {
     console.log(error.message);
